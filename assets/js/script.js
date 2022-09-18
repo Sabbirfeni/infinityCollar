feb5701813f68dd326f1fb120b4987875bfe75b9
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-09-18 12:57:22 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// ********************************
+// Sidebar js start
+// ********************************
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+// ********************************
+// Sidebar js end
+// ********************************
+
+
+
+
+
+// ********************************
+// Side form js start
+// ********************************
+
+
+function openContact() {
+  document.getElementById("sideContact").style.width = "350px";
+}
+function closeContact() {
+  document.getElementById("sideContact").style.width = "0";
+}
+
+
+// ********************************
+// Side form js end
+// ********************************
+
+
