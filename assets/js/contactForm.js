9cc35575d7e413862d6ab36c3fdbf58ea77234fb
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-09-22 12:13:36 +0600

    form submit function added successfully

--- assets/js/contactForm.js
+++ assets/js/contactForm.js
@@ -1,14 +1,17 @@
-var fullName = document.getElementById('fullName');
-var email = document.getElementById('email');
-var phoneNumber = document.getElementById('phoneNumber');
-var query = document.getElementById('query');
+
 var submitBtn = document.getElementById('submit');
 var alertMsg =document.getElementById('alertMsg');
 var model = document.getElementById('exampleModal');
 
-fullName = email = phoneNumber = query = '';
 
 submitBtn.addEventListener('click', () => {
+
+    var fullName = document.getElementById('fullName').value;
+    var email = document.getElementById('email').value;
+    var phoneNumber = document.getElementById('phoneNumber').value;
+    var query = document.getElementById('query').value;
+
+
     if(fullName == '' || email == '' || phoneNumber == '' || query == '') {
         alertMsg.innerText = 'Please enter all data!';
         alertMsg.style.color = 'red';
@@ -16,4 +19,5 @@ submitBtn.addEventListener('click', () => {
         alertMsg.innerText = "Thanks! We look forward to sharing more about our products with you.";
         alertMsg.style.color = 'green';
     }
+    
 })
