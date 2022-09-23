400e32c34a786351c306d6dbfa764f69e866e35f
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-09-23 12:42:58 +0600

    form updated

--- assets/js/contactForm.js
+++ assets/js/contactForm.js
@@ -1,10 +1,14 @@
 
 var submitBtn = document.getElementById('submit');
 var alertMsg =document.getElementById('alertMsg');
+var modelContainer = document.getElementById('exampleModal');
 var model = document.getElementById('exampleModal');
+var modalBackDrop = document.getElementsByClassName('model-open');
+var successMsg = document.getElementById('successMsg');
 
 
 submitBtn.addEventListener('click', () => {
+
 
     var fullName = document.getElementById('fullName').value;
     var email = document.getElementById('email').value;
@@ -13,11 +17,31 @@ submitBtn.addEventListener('click', () => {
 
 
     if(fullName == '' || email == '' || phoneNumber == '' || query == '') {
+        
         alertMsg.innerText = 'Please enter all data!';
         alertMsg.style.color = 'red';
+
     } else {
+
         alertMsg.innerText = "Thanks! We look forward to sharing more about our products with you.";
         alertMsg.style.color = 'green';
+
+
+
+
+        // successAlert();
+
+        // function successAlert() {
+        //     successMsg.classList.add('submitSuccessAlert');
+        //     modelContainer.remove();
+        //     modalBackDrop.remove();
+        // }
+
+        // setTimeout(() => {
+        //     successMsg.classList.remove('submitSuccessAlert');
+        // }, 5000);
+
+
     }
     
 })
