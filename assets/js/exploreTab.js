92d4ea355529f980792bfc56d29ef592fd511fcf
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-11-04 01:48:49 +0600

    All pages change direction with the figma file

---
+++ assets/js/exploreTab.js
@@ -0,0 +1,36 @@
+
+
+
+function openTab(tab, tabName) {
+    var i, tabcontent, tabLinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    tabLinks = document.getElementsByClassName("tabLink");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        tabLinks[i].style.borderBottom = 'none';
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    document.getElementById(tab).style.borderBottom = "2px solid #5B5B5B";
+}
+
+document.getElementById("defaultOpen").click();
+
+
+
+
+
+
+
+
+
+
+// console.log(tabLinks)
+
+// tabLinks.forEach((value, index) => {
+   
+//     value.addEventListener('click', () => {
+       
+        
+//     })
+// })
